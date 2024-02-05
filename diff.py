933e62981221ebe7e0db5c0775f73e243b423d19
DDIDUs <92288706+DDIDUs@users.noreply.github.com>
2024-02-05 19:20:35 +0900

    Update diff.py

--- diff.py
+++ diff.py
@@ -53,9 +53,9 @@ dataset_load_func = {
 def load_testdata(datasets):
     test_transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
-    test_dataset = dataset_load_func[datasets]("./data", train=True, download=True, transform=test_transform)
+    test_dataset = dataset_load_func[datasets]("./data", train=False, download=True, transform=test_transform)
     test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=4)
     y_test = [i for _, i in test_dataset]
     return test_dataloader, y_test
